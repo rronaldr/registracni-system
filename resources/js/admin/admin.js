f99be90db63a2fb680f5229c36c1e0290ef29308
Ronald Rebernigg <ronaldrronaldr@gmail.com>
2023-07-09 16:39:22 +0200

    resources mix changes

--- resources/js/admin/admin.js
+++ resources/js/admin/admin.js
@@ -21,3 +21,19 @@
     // });
 })();
 
+
+// Initiate datatables in roles, tables, users page
+(function() {
+    'use strict';
+
+    $('#dataTables-example').DataTable({
+        responsive: true,
+        pageLength: 20,
+        lengthChange: false,
+        searching: true,
+        ordering: true,
+        paging: false,
+    });
+})();
+
+
