6f7be2942b8365cee341378dbda23cdb47432ba1
Ronald Rebernigg <ronaldrronaldr@gmail.com>
2023-07-25 16:02:04 +0200

    TinyMCE 6 upgrade

--- resources/views/components/wysiwyg.blade.php
+++ resources/views/components/wysiwyg.blade.php
@@ -1,4 +1,8 @@
-<script src="https://cdn.tiny.cloud/1/6kz4ky7ixdl549cg6caz3f4hnjl4vhwa4f3ixy4qjrol04a5/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+<template>
+
+</template>
+
+<script src="https://cdn.tiny.cloud/1/6kz4ky7ixdl549cg6caz3f4hnjl4vhwa4f3ixy4qjrol04a5/tinymce/6/tinymce.min.js" referrerpolicy="origin"></script>
 <script>
     tinymce.init({
         selector: '.wysiwyg',
@@ -6,15 +10,13 @@
         height: 400,
         branding: false,
         menubar: false,
-        plugins: 'searchreplace autolink visualchars fullscreen image link media codesample table charmap hr nonbreaking anchor insertdatetime advlist lists textcolor wordcount imagetools contextmenu colorpicker textpattern code paste',
+        plugins: 'searchreplace autolink visualchars fullscreen image link media codesample table charmap nonbreaking anchor insertdatetime advlist lists wordcount code',
         toolbar: 'insert | undo redo |  formatselect | bold italic strikethrough | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link unlink removeformat code',
         image_advtab: true,
         //paste_as_text: true
         paste_auto_cleanup_on_paste: true,
-        paste_word_valid_elements: "b,strong,i,em,h1,h2,a",
         paste_remove_spans: true,
         paste_remove_styles: true,
-        paste_retain_style_properties: "color font-size",
         paste_strip_class_attributes: 'all'
     });
 
