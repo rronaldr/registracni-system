ad5b91fc84e797b90b2740da72c99430e505c412
Ronald Rebernigg <ronaldrronaldr@gmail.com>
2023-11-28 18:43:30 +0100

    Remove permission listing on user page

--- resources/views/admin/user-roles.blade.php
+++ resources/views/admin/user-roles.blade.php
@@ -4,18 +4,6 @@
     <div class="row justify-content-center">
         <div class="col-lg-12 col-md-10 col-sm-8">
             <roles-page :roles="'{{ $roles }}'"></roles-page>
-
-            <hr class="line">
-
-            <h4>Roles and permissons</h4>
-            @foreach($roles as $role)
-                <h5>{{ $role->name }}</h5>
-                <span class="small">
-                            @foreach($role->permissions as $permission)
-                        <span class="small">{{ $permission->name }} | </span>
-                    @endforeach
-                        </span>
-            @endforeach
         </div>
     </div>
 @endsection
