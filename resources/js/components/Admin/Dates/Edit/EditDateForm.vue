5ac26e9f1533aa16caa729fc080850b65dfd3000
Ronald Rebernigg <ronaldrronaldr@gmail.com>
2023-12-02 22:51:10 +0100

    Fix wrong translation

--- resources/js/components/Admin/Dates/Edit/EditDateForm.vue
+++ resources/js/components/Admin/Dates/Edit/EditDateForm.vue
@@ -200,7 +200,7 @@
                         </div>
                     </div>
 
-                    <SubmitButton />
+                    <SubmitButton label="date.save" />
                 </div>
             </form>
         </div>
