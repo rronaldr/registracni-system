fb9d227757fe8ddd9c86de6c08124d51bb9d63aa
Ronald Rebernigg <ronaldrronaldr@gmail.com>
2024-03-10 17:33:01 +0100

    Update iframe navbar

--- resources/views/layouts/iframe/navbar.blade.php
+++ resources/views/layouts/iframe/navbar.blade.php
@@ -1,11 +1,11 @@
 <body class="bg-gray-500">
-<header class="header-main bg-primary bg-cover">
+<header class="bg-header-xl-dark bg-menu header-fixed-mobile text-white">
     <div class="container">
-        <nav class="navbar-top d-xl-flex justify-content-between align-self-start">
-            <ul class="nav">
+        <nav class="navbar-top d-flex align-items-center justify-content-between">
+            <ul class="nav nav-system align-items-stretch ml-auto">
                 @guest()
                     @if (Route::has('login'))
-                        <li class="nav-item">
+                        <li class="nav-item nav-item-light">
                             <a class="nav-link" target="_blank" href="{{ route('iframe.login.index') }}">
                                 <i class="icon icon-user"></i>
                                 {{ __('app.auth.login') }}
@@ -13,12 +13,12 @@
                         </li>
                     @endif
                 @else
-                    <li class="nav-item">
+                    <li class="nav-item nav-item-light">
                         <p class="text-white">
                             <i class="icon icon-user"></i> <span>{{ auth()->user()->getFullname() }}</span>
                         </p>
                     </li>
-                    <li class="nav-item">
+                    <li class="nav-item nav-item-light">
                         <a class="nav-link"
                            href="@if(isset(auth()->user()->xname)) {{ route('iframe.logout') }} @else {{ route('iframe.logout') }}@endif"
                         >{{ __('app.auth.logout') }}</a>
